e9e678322ab2e49632c2d4008ed9be7a62625df0
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2024-01-25 01:18:35 +0530

    another commit

--- weeks/week6/week6.1(hooks)/src/App.jsx
+++ weeks/week6/week6.1(hooks)/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import React, { memo } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [data , setData] = useState([]);
+
+  useEffect(()=>{
+    async function dataReciever(){
+      try{
+        const response = await fetch('https://api.jikan.moe/v4/anime');
+        if(!response.ok){
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const data = await response.json();
+        setData(data.data);
+      }
+      catch(error){
+        console.error('Error' , error.message)
+      }
+    }
+
+    dataReciever()
+  },[])
+
+  function Card({title , synopsis , score , image}){
+    return (
+      <div className='p-3 border m-4 rounded shadow'>
+      <h1 className="text-3xl text-red-500 ">{title}</h1>
+      <img src={image} />
+      <p>{score}</p>
+      
+      
+    </div>
+    )
+  }
+
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <h1 className='text-center mt-4 text-4xl'>Anime</h1>
+    {
+      console.log(data)
+    }
+     {data.map((cardData)=>{
+      return <Card title={cardData.title}  key={cardData.mal_id} score={cardData.score} image={cardData.images.jpg.image_url}/>
+     })}
     </>
-  )
+  );
+
+  
+
 }
 
-export default App
+
+
+export default App;
