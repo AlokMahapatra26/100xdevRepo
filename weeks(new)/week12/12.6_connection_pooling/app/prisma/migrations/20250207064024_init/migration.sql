ed4de560f5332434962918777787ef148f9c15cb
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2025-02-07 13:25:50 +0530

    week 12

---
+++ weeks(new)/week12/12.6_connection_pooling/app/prisma/migrations/20250207064024_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Log" (
+    "id" SERIAL NOT NULL,
+    "message" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
+);
